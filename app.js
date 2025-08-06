702a61a2ae8073cbd48fa6b2ecf34598e60a1758
Austin Moore <133458852+Null-Austin@users.noreply.github.com>
2025-08-06 15:41:09 -0500

    added a coolio update

--- app.js
+++ app.js
@@ -1,2 +1,26 @@
-const express = require('express')
+// init
+const port = process.env.trackerport || 3000
 
+// packages
+const express = require('express')
+const path = require('node:path')
+const _db = require('better-sqlite3')
+
+// setting up db
+let db = _db(path.join(__dirname,'db.db'))
+db.prepare(`CREATE table IF NOT EXISTS users (uuid,ips,unique_views,views)`).run()
+
+// set up app
+const app = express()
+
+// endpoints
+app.get('/css/views/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'stylesheet.css'))
+})
+
+app.listen(port,err=>{
+    if (err){
+        return console.error(err)
+    }
+    return console.log("Server started! http://localhost:"+port)
+})
